81ed58092fbd8375f484d9d7fefc083565217e8d
Ron Zhong <ron.zhong@gmail.com>
2018-12-02 09:42:52 +0800

    hello ron

--- Src/Ch01/HelloWorldWeb/Startup.cs
+++ Src/Ch01/HelloWorldWeb/Startup.cs
@@ -18,7 +18,7 @@ namespace Ch01.HelloWorldWeb
         {
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Ron!");
             });
         }
     }
